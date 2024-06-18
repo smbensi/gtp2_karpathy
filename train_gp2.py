e55cee7ea6ab21250bd8917a3e215b28de99d05e
mathias <mathiasbensimhon@gmail.com>
2024-06-18 16:09:15 +0300

    adding Block

--- train_gp2.py
+++ train_gp2.py
@@ -3,6 +3,8 @@ import torch
 import torch.nn as nn
 from torch.nn import functional as F
 
+    
+
 @dataclass
 class GPTConfig:
     block_size: int = 256
@@ -11,6 +13,22 @@ class GPTConfig:
     n_head: int = 6
     n_embd: int = 384
     
+class Block(nn.Module):
+    
+    def __init__(self, config:GPTConfig):
+        super().__init__()
+        self.ln_1 = nn.LayerNorm(config.n_embd)
+        self.attn = CausalSelfAttention(config)
+        self.ln_2 = nn.LayerNorm(config.n_embd)
+        self.mlp = MLP(config)
+    
+    def forward(self, x):
+        x = x + self.attn(self.ln_1(x))
+        x = x + self.mlp(self.ln_2(x))
+        return x
+        
+        
+        
 class GPT(nn.Module):
     """GPT is a decoder-only transformer"""
     
@@ -19,11 +37,12 @@ class GPT(nn.Module):
         self.config = config
     
         self.transformer = nn.ModuleDict(dict(
-            wte = nn.Embedding(config.vocab_size, config.n_embd),
-            wpe = nn.Embedding(config.block_size, config.n_embd),
+            wte = nn.Embedding(config.vocab_size, config.n_embd), #token embedding
+            wpe = nn.Embedding(config.block_size, config.n_embd),  # position encodding
             h = nn.ModuleList([Block(config) for  _ in range(config.n_layer)]),
             ln_f = nn.LayerNorm(config.n_embd),
         ))
         
         self.lm_head = nn.Linear(config.n_embd, config.vocab_size, bias=False)
-        
+
+
