90deaa318f3a74f76d97ad3eb2f36f1d8b847dae
Mathias Bensimhon <mathiasbensimhon@Mathiass-MacBook-Pro.local>
2024-06-18 22:37:45 +0300

    adding MLP

--- train_gp2.py
+++ train_gp2.py
@@ -13,6 +13,20 @@ class GPTConfig:
     n_head: int = 6
     n_embd: int = 384
     
+class MLP(nn.Module):
+    
+    def __init__(self, config):
+        super().__init__()
+        self.c_fc   = nn.Linear(config.n_embd, 4 * config.n_embd)
+        self.gelu   = nn.GELU(approximate='tanh')
+        self.c_proj = nn.Linear(4 * config.n_embd, config.n_embd)
+        
+    def forward(self, x):
+        x = self.c_fc(x)
+        x = self.gelu(x)
+        x = self.c_proj(x)
+        return x 
+        
 class Block(nn.Module):
     
     def __init__(self, config:GPTConfig):
